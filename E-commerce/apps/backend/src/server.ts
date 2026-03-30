1bc1717c0ceb493b35d5bd6d035dfb6239d78901
rrc_f2024_abansal2 <abansal2@rrc.ca>
2026-03-30 13:11:25 -0500

    setup docker

--- E-commerce/apps/backend/src/server.ts
+++ E-commerce/apps/backend/src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
+  res.send("Backend running");
 });
 
 app.listen(PORT, () => {

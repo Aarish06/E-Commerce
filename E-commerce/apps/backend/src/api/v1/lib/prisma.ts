bb82e98b541c7a2a899b10995edf00620affd2a2
Navkaran7007 <nsidhu10@rrc.ca>
2026-04-23 12:57:03 -0500

    Add Clerk SDK and auth middleware

--- E-commerce/apps/backend/src/api/v1/lib/prisma.ts
+++ E-commerce/apps/backend/src/api/v1/lib/prisma.ts
@@ -1,10 +1,10 @@
-//@ts-ignore
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const prisma = new PrismaClient({ adapter });
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export { prisma };
